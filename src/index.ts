d1055a39b545fd3597a9b8ea0d000cefaac205dc
Alphonse <batmandocode@gmail.com>
2024-11-21 20:06:52 +1100

    UPDATE: update the sample response body

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ const port = process.env.port || 3000;
 const version = process.env.version || '0.0.5';
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.json({
+    message: 'Hello World!',
+  });
 });
 
 const server = app.listen(port, () => {
