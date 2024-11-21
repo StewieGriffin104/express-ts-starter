d24a5a0d1ae3ea995e99d0c5ef50e212162665d1
Alphonse <batmandocode@gmail.com>
2024-11-21 20:09:31 +1100

    UPDATE: added helmate and cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response } from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.port || 3000;
 const version = process.env.version || '0.0.5';
+
+app.use(helmet());
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.json({
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -9,6 +9,5 @@ describe('GET /', () => {
   it('should return Hello World!', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello World!');
   });
 });
